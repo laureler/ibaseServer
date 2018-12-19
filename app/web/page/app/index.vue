d6838cea826ca05c4ea43eb5d8a0ebf36dd48f38
yunlong <laureler@163.com>
2018-12-19 15:39:21 +0800

    1. 添加初始化配置说明 2. 添加国际化

--- app/web/page/app/index.vue
+++ app/web/page/app/index.vue
@@ -1,18 +1,32 @@
 <template>
-  <Layout>
-    <transition name="fade" mode="out-in">
-      <router-view></router-view>
-    </transition>
-  </Layout>
+	<Layout>
+		<transition name="fade" mode="out-in">
+			<router-view></router-view>
+		</transition>
+	</Layout>
 </template>
 <script type="text/babel">
-  import Layout from 'component/layout/app/index.vue';
-  export default {
-    components: {
-      Layout
-    },
-    mounted(){
+	// 引入vue.js
+	import * as Vue from "vue";
 
-    }
-  }
+	// 引入elementui.js
+	import * as ElementUI from "element-ui";
+
+	// 引入elementui.css
+	import 'element-ui/lib/theme-chalk/index.css';
+
+	//引入 layout根部布局
+	import Layout from 'component/layout/app/index.vue';
+
+	//加载elementui
+	Vue.use(ElementUI)
+
+	export default {
+		components: {
+			Layout
+		},
+		mounted() {
+
+		}
+	}
 </script>
