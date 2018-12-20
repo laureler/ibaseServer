78ec1ce1f31460426990a3291f4c1bc26a31dc19
yunlong <laureler@163.com>
2018-12-20 13:56:09 +0800

    添加重定向

--- app/web/page/app/ibasePlugin/common/http.js
+++ app/web/page/app/ibasePlugin/common/http.js
@@ -20,10 +20,31 @@ import axios from 'axios'
 });*/
 
 let activeId = sessionStorage.getItem('activeId');
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  if(response.status == 302){
+    debugger
+    if(window.parent){
+      window.parent.location.href = "/cas/login"
+    }
+    window.location.href = "/cas/login"
+  }
+  return response;
+}, function (error) {
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
 export default {
-  newPost(url,json){
-    return axios.post(url,json)
-  },
   fetch: (url,params={})=>{
     return new Promise((resolve,reject) => {
       axios.get(url,{
